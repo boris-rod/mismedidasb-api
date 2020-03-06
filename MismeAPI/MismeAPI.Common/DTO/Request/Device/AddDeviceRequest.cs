469d9cb73e1df19aae5ab774f22c991e86e725d3
Yoandry <mazinchess@gmail.com>
2020-03-06 16:30:13 -0500

    Revert "Revert "Add device endpoint.""
    This reverts commit 2d92e14b8c6830915fd2437e8d0ae889d3a05650.

---
+++ MismeAPI/MismeAPI.Common/DTO/Request/Device/AddDeviceRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MismeAPI.Common.DTO.Request.Device
+{
+    public class AddDeviceRequest
+    {
+        public int DeviceId { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ MismeAPI/MismeAPI.Service/IDeviceService.cs
@@ -0,0 +1,10 @@
+﻿using MismeAPI.Common.DTO.Request.Device;
+using System.Threading.Tasks;
+
+namespace MismeAPI.Service
+{
+    public interface IDeviceService
+    {
+        Task CreateOrUpdateDeviceAsync(int loggedUser, AddDeviceRequest device);
+    }
+}
