724ded404c7ee11042f7664cdd4a64dcff02f635
Pavel <pavel.mena@ntsprint.com>
2020-06-09 15:23:54 -0400

    Bugfixing

--- MismeAPI/MismeAPI.Common/DTO/Response/CompoundDish/CompoundDishResponse.cs
+++ MismeAPI/MismeAPI.Common/DTO/Response/CompoundDish/CompoundDishResponse.cs
@@ -16,7 +16,7 @@ namespace MismeAPI.Common.DTO.Response.CompoundDish
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
         public bool IsDeleted { get; set; }
-        public DateTime DeletedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
         public ICollection<DishCompoundDishResponse> DishCompoundDishResponse { get; set; }
 
         public double Calories { get; set; }
