c24108003475b305a9147627089fdfe413512293
pmena90 <pavel.mena1990@gmail.com>
2020-06-29 10:42:32 -0400

    Optimice login and refresh token endpoints.

--- MismeAPI/MismeAPI.Service/Impls/AccountService.cs
+++ MismeAPI/MismeAPI.Service/Impls/AccountService.cs
@@ -100,10 +100,155 @@ namespace MismeAPI.Services.Impls
             await _uow.UserTokenRepository.AddAsync(t);
             await _uow.CommitAsync();
 
-            var kcal = GetKCal(user.Id);
-            var imc = GetIMC(user.Id);
-            var first = GetFirstHealthMeasured(user.Id);
+            var info = await GetKCalIMCFirstHeltMeasureAsync(user.Id);
+
+            var kcal = info.kcal;
+            var imc = info.imc;
+            var first = info.firstHealthMeasure;
+
             return (user, token, refreshToken, kcal, imc, first);
+        }
+
+        private async Task<(double kcal, double imc, DateTime? firstHealthMeasure)> GetKCalIMCFirstHeltMeasureAsync(int userId)
+        {
+            try
+            {
+                var concept = await _uow.ConceptRepository.FindAsync(c => c.Codename == CodeNamesConstants.HEALTH_MEASURES);
+
+                if (concept != null)
+                {
+                    var polls = await _uow.PollRepository.GetAll().Where(p => p.ConceptId == concept.Id)
+                          .Include(p => p.Questions)
+                          .OrderBy(p => p.Order)
+                          .ToListAsync();
+                    // this is hardcoded but is the way it is.
+
+                    // poll 1- personal data
+                    var questions = polls.ElementAt(0).Questions.OrderBy(q => q.Order);
+
+                    var age = 0;
+                    var weight = 0;
+                    var height = 0;
+                    var sex = 0;
+                    DateTime? firstHealthMeasure = null;
+
+                    var count = 0;
+                    foreach (var q in questions)
+                    {
+                        var ua = await _uow.UserAnswerRepository.GetAll().Where(u => u.UserId == userId && u.Answer.QuestionId == q.Id)
+                            .Include(u => u.Answer)
+                                .ThenInclude(a => a.Question)
+                            .OrderByDescending(ua => ua.CreatedAt)
+                            .FirstOrDefaultAsync();
+                        //age
+                        if (count == 0 && ua != null)
+                        {
+                            age = ua.Answer.Weight;
+                        }
+                        //weight
+                        else if (count == 1 && ua != null)
+                        {
+                            weight = ua.Answer.Weight;
+                        }
+                        //height
+                        else if (count == 2 && ua != null)
+                        {
+                            height = ua.Answer.Weight;
+                        }
+                        //sex
+                        else
+                        {
+                            sex = ua.Answer.Weight;
+                        }
+
+                        count += 1;
+                    }
+
+                    //poll 2- Physical excersice
+                    var physicalExercise = 0;
+                    var physicalQuestion = polls.ElementAt(1).Questions.OrderBy(q => q.Order).FirstOrDefault();
+                    if (physicalQuestion != null)
+                    {
+                        var ua = await _uow.UserAnswerRepository.GetAll().Where(u => u.UserId == userId && u.Answer.QuestionId == physicalQuestion.Id)
+                                .Include(u => u.Answer)
+                                    .ThenInclude(a => a.Question)
+                                .OrderByDescending(ua => ua.CreatedAt)
+                                .FirstOrDefaultAsync();
+                        if (ua != null)
+                        {
+                            physicalExercise = ua.Answer.Weight;
+                        }
+                    }
+
+                    // other values
+                    var c = await _uow.UserConceptRepository.GetAll().Where(c => c.UserId == userId && c.ConceptId == concept.Id).FirstOrDefaultAsync();
+
+                    var IMC = Convert.ToDouble(weight) / ((Convert.ToDouble(height) / 100) * ((Convert.ToDouble(height) / 100)));
+                    var TMB_PROV = 10 * Convert.ToDouble(weight) + 6.25 * Convert.ToDouble(height) - 5 * age;
+
+                    var dailyKalDouble = 0.0;
+
+                    if (sex == 1)
+                    {
+                        if (physicalExercise == 1)
+                        {
+                            dailyKalDouble = (TMB_PROV + 5) * 1.2;
+                        }
+                        else if (physicalExercise == 2)
+                        {
+                            dailyKalDouble = (TMB_PROV + 5) * 1.375;
+                        }
+                        else if (physicalExercise == 3)
+                        {
+                            dailyKalDouble = (TMB_PROV + 5) * 1.55;
+                        }
+                        else if (physicalExercise == 4)
+                        {
+                            dailyKalDouble = (TMB_PROV + 5) * 1.725;
+                        }
+                        else
+                        {
+                            dailyKalDouble = (TMB_PROV + 5) * 1.9;
+                        }
+                    }
+                    else
+                    {
+                        if (physicalExercise == 1)
+                        {
+                            dailyKalDouble = (TMB_PROV - 161) * 1.2;
+                        }
+                        else if (physicalExercise == 2)
+                        {
+                            dailyKalDouble = (TMB_PROV - 161) * 1.375;
+                        }
+                        else if (physicalExercise == 3)
+                        {
+                            dailyKalDouble = (TMB_PROV - 161) * 1.55;
+                        }
+                        else if (physicalExercise == 4)
+                        {
+                            dailyKalDouble = (TMB_PROV - 161) * 1.725;
+                        }
+                        else
+                        {
+                            dailyKalDouble = (TMB_PROV - 161) * 1.9;
+                        }
+                    }
+
+                    if (c != null)
+                    {
+                        firstHealthMeasure = c.CompletedAt;
+                    }
+
+                    return (dailyKalDouble, IMC, firstHealthMeasure);
+                }
+            }
+            catch (Exception)
+            {
+                return (0.0, 0.0, null);
+            }
+
+            return (0.0, 0.0, null);
         }
 
         private double GetKCal(int userId)
@@ -111,12 +256,13 @@ namespace MismeAPI.Services.Impls
             try
             {
                 var concept = _uow.ConceptRepository.GetAll().Where(c => c.Codename == CodeNamesConstants.HEALTH_MEASURES).FirstOrDefault();
+
                 if (concept != null)
                 {
                     var polls = _uow.PollRepository.GetAll().Where(p => p.ConceptId == concept.Id)
-                      .Include(p => p.Questions)
-                      .OrderBy(p => p.Order)
-                      .ToList();
+                          .Include(p => p.Questions)
+                          .OrderBy(p => p.Order)
+                          .ToList();
                     // this is hardcoded but is the way it is.
 
                     // poll 1- personal data
@@ -175,22 +321,22 @@ namespace MismeAPI.Services.Impls
                         }
                     }
                     //poll 3- Diet
-                    var dietSummary = 0;
-                    questions = polls.ElementAt(2).Questions.OrderBy(q => q.Order);
+                    //var dietSummary = 0;
+                    //questions = polls.ElementAt(2).Questions.OrderBy(q => q.Order);
 
-                    foreach (var q in questions)
-                    {
-                        var ua = _uow.UserAnswerRepository.GetAll().Where(u => u.UserId == userId && u.Answer.QuestionId == q.Id)
-                            .Include(u => u.Answer)
-                                .ThenInclude(a => a.Question)
-                            .OrderByDescending(ua => ua.CreatedAt)
-                            .FirstOrDefault();
+                    //foreach (var q in questions)
+                    //{
+                    //    var ua = _uow.UserAnswerRepository.GetAll().Where(u => u.UserId == userId && u.Answer.QuestionId == q.Id)
+                    //        .Include(u => u.Answer)
+                    //            .ThenInclude(a => a.Question)
+                    //        .OrderByDescending(ua => ua.CreatedAt)
+                    //        .FirstOrDefault();
 
-                        dietSummary += ua.Answer.Weight;
-                    }
+                    //    dietSummary += ua.Answer.Weight;
+                    //}
 
                     // other values
-                    var IMC = Convert.ToDouble(weight) / ((Convert.ToDouble(height) / 100) * ((Convert.ToDouble(height) / 100)));
+                    //var IMC = Convert.ToDouble(weight) / ((Convert.ToDouble(height) / 100) * ((Convert.ToDouble(height) / 100)));
                     var TMB_PROV = 10 * Convert.ToDouble(weight) + 6.25 * Convert.ToDouble(height) - 5 * age;
 
                     var dailyKalDouble = 0.0;
@@ -272,12 +418,13 @@ namespace MismeAPI.Services.Impls
             try
             {
                 var concept = _uow.ConceptRepository.GetAll().Where(c => c.Codename == CodeNamesConstants.HEALTH_MEASURES).FirstOrDefault();
+
                 if (concept != null)
                 {
                     var polls = _uow.PollRepository.GetAll().Where(p => p.ConceptId == concept.Id)
-                      .Include(p => p.Questions)
-                      .OrderBy(p => p.Order)
-                      .ToList();
+                          .Include(p => p.Questions)
+                          .OrderBy(p => p.Order)
+                          .ToList();
                     // this is hardcoded but is the way it is.
 
                     // poll 1- personal data
@@ -501,7 +648,7 @@ namespace MismeAPI.Services.Impls
             }
             if (refToken.RefreshToken != refreshToken.RefreshToken)
             {
-                throw new MismeAPI.Common.Exceptions.InvalidDataException(ExceptionConstants.INVALID_DATA, "refresh token.");
+                throw new InvalidDataException(ExceptionConstants.INVALID_DATA, "refresh token.");
             }
         }
 
@@ -526,7 +673,7 @@ namespace MismeAPI.Services.Impls
             oldToken.RefreshToken = newRefreshToken;
             oldToken.RefreshTokenExpiresDateTime = DateTime.UtcNow.AddHours(int.Parse(_configuration.GetSection("BearerTokens")["RefreshTokenExpirationHours"]));
 
-            _uow.UserTokenRepository.Update(oldToken);
+            await _uow.UserTokenRepository.UpdateAsync(oldToken, oldToken.Id);
             await _uow.CommitAsync();
             return (newToken, newRefreshToken);
         }
