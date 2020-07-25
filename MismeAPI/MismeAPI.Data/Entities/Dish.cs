8997c236aed649d036ccbc076b08a6f17011c988
Yoandry <mazinchess@gmail.com>
2020-07-24 20:50:20 -0400

    Add new dish fields.

--- MismeAPI/MismeAPI.Data/Entities/Dish.cs
+++ MismeAPI/MismeAPI.Data/Entities/Dish.cs
@@ -18,33 +18,43 @@ namespace MismeAPI.Data.Entities
         public string Name { get; set; }
         public string NameEN { get; set; }
         public string NameIT { get; set; }
-        public double Calories { get; set; }
-        public double Carbohydrates { get; set; }
-        public double Proteins { get; set; }
-        public double Fat { get; set; }
-        public double Fiber { get; set; }
+        public double? Calories { get; set; }
+        public double? Carbohydrates { get; set; }
+        public double? Proteins { get; set; }
+        public double? Fat { get; set; }
+        public double? Fiber { get; set; }
         public string Image { get; set; }
         public string ImageMimeType { get; set; }
         public bool IsProteic { get; set; }
         public bool IsCaloric { get; set; }
         public bool IsFruitAndVegetables { get; set; }
-        public double Cholesterol { get; set; }
-        public double Calcium { get; set; }
-        public double Phosphorus { get; set; }
-        public double Iron { get; set; }
-        public double Potassium { get; set; }
-        public double Sodium { get; set; }
-        public double Zinc { get; set; }
-        public double Magnesium { get; set; }
-        public double Thiamine { get; set; }
-        public double Ribofla { get; set; }
-        public double Niacin { get; set; }
-        public double FolicAcid { get; set; }
-        public double VitaminA { get; set; }
-        public double VitaminC { get; set; }
-        public double VitaminB6 { get; set; }
-        public double VitaminB12 { get; set; }
+        public double? Cholesterol { get; set; }
+        public double? Calcium { get; set; }
+        public double? Phosphorus { get; set; }
+        public double? Iron { get; set; }
+        public double? Potassium { get; set; }
+        public double? Sodium { get; set; }
+        public double? Zinc { get; set; }
+        public double? Magnesium { get; set; }
+        public double? FolicAcid { get; set; }
+        public double? VitaminA { get; set; }
+        public double? VitaminC { get; set; }
+        public double? VitaminD { get; set; }
+        public double? VitaminE { get; set; }
+        public double? VitaminK { get; set; }
+        public double? VitaminB1Thiamin { get; set; }
+        public double? VitaminB2Riboflavin { get; set; }
+        public double? VitaminB3Niacin { get; set; }
+        public double? VitaminB6 { get; set; }
+        public double? VitaminB9Folate { get; set; }
+        public double? VitaminB12 { get; set; }
 
+        public string Code { get; set; }
+        public double? NetWeight { get; set; }
+        public double? Volume { get; set; }
+        public double? SaturatedFat { get; set; }
+        public double? MonoUnsaturatedFat { get; set; }
+        public double? PolyUnsaturatedFat { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
         public virtual ICollection<DishTag> DishTags { get; set; }
---
+++ MismeAPI/MismeAPI.Data/Migrations/20200725004917_NewDishFields.cs
@@ -0,0 +1,433 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MismeAPI.Data.Migrations
+{
+    public partial class NewDishFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Niacin",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "Ribofla",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "Thiamine",
+                table: "dish");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Zinc",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminC",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminB6",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminB12",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminA",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Sodium",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Proteins",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Potassium",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Phosphorus",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Magnesium",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Iron",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "FolicAcid",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Fiber",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Fat",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Cholesterol",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Carbohydrates",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Calories",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Calcium",
+                table: "dish",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "MonoUnsaturatedFat",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "NetWeight",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PolyUnsaturatedFat",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "SaturatedFat",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminB1Thiamin",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminB2Riboflavin",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminB3Niacin",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminB9Folate",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminD",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminE",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "VitaminK",
+                table: "dish",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Volume",
+                table: "dish",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "MonoUnsaturatedFat",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "NetWeight",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "PolyUnsaturatedFat",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "SaturatedFat",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminB1Thiamin",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminB2Riboflavin",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminB3Niacin",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminB9Folate",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminD",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminE",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "VitaminK",
+                table: "dish");
+
+            migrationBuilder.DropColumn(
+                name: "Volume",
+                table: "dish");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Zinc",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminC",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminB6",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminB12",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "VitaminA",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Sodium",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Proteins",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Potassium",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Phosphorus",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Magnesium",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Iron",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "FolicAcid",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Fiber",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Fat",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Cholesterol",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Carbohydrates",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Calories",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Calcium",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Niacin",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Ribofla",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Thiamine",
+                table: "dish",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
