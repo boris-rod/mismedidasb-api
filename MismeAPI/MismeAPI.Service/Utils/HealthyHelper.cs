f281888b091bd3e7812ba449eb8db340cb78b0f9
pmena90 <pavel.mena1990@gmail.com>
2020-09-08 00:15:54 -0400

    Add support to calculate IsBalanced in the server side.

--- MismeAPI/MismeAPI.Service/Utils/HealthyHelper.cs
+++ MismeAPI/MismeAPI.Service/Utils/HealthyHelper.cs
@@ -1,5 +1,6 @@
 ﻿using MismeAPI.Common.DTO.Response.User;
 using MismeAPI.Data.Entities;
+using MismeAPI.Data.Entities.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -37,6 +38,127 @@ namespace MismeAPI.Service.Utils
             };
         }
 
+        public bool IsBalancedPlan(User user, IEnumerable<Eat> dayPlan)
+        {
+            var parameters = GetUserEatHealtParameters(user);
+
+            double currentCaloriesSum = 0;
+            double currentProteinsSum = 0;
+            double currentCarbohydratesSum = 0;
+            double currentFatSum = 0;
+            double currentFiberSum = 0;
+
+            bool breakfastKcal = false;
+            bool snack1Kcal = false;
+            bool lunchKcal = false;
+            bool snack2Kcal = false;
+            bool dinnerKcal = false;
+
+            foreach (var eat in dayPlan)
+            {
+                double eatTypeCalories = 0;
+
+                foreach (var eatDish in eat.EatDishes)
+                {
+                    var dishCals = eatDish.Dish.Calories.HasValue ? eatDish.Dish.Calories.Value : 0;
+                    var calories = dishCals * eatDish.Qty;
+                    eatTypeCalories += calories;
+
+                    var dishProteins = eatDish.Dish.Proteins.HasValue ? eatDish.Dish.Proteins.Value : 0;
+                    var proteins = dishProteins * eatDish.Qty;
+                    currentProteinsSum += proteins;
+
+                    var dishCarbs = eatDish.Dish.Carbohydrates.HasValue ? eatDish.Dish.Carbohydrates.Value : 0;
+                    var carbohydrates = dishCarbs * eatDish.Qty;
+                    currentCarbohydratesSum += carbohydrates;
+
+                    var dishFats = eatDish.Dish.Fat.HasValue ? eatDish.Dish.Fat.Value : 0;
+                    var fats = dishFats * eatDish.Qty;
+                    currentFatSum += fats;
+
+                    var dishFibs = eatDish.Dish.Fiber.HasValue ? eatDish.Dish.Fiber.Value : 0;
+                    var fibers = dishFibs * eatDish.Qty;
+                    currentFiberSum += fibers;
+                }
+
+                foreach (var eatCompoundDish in eat.EatCompoundDishes)
+                {
+                    foreach (var dishCompoundDish in eatCompoundDish.CompoundDish.DishCompoundDishes)
+                    {
+                        var dishCals = dishCompoundDish.Dish.Calories.HasValue ? dishCompoundDish.Dish.Calories.Value : 0;
+                        var calories = dishCals * eatCompoundDish.Qty * dishCompoundDish.DishQty;
+                        eatTypeCalories = calories;
+
+                        var dishProteins = dishCompoundDish.Dish.Proteins.HasValue ? dishCompoundDish.Dish.Proteins.Value : 0;
+                        var proteins = dishProteins * eatCompoundDish.Qty * dishCompoundDish.DishQty;
+                        currentProteinsSum += proteins;
+
+                        var dishCarbs = dishCompoundDish.Dish.Carbohydrates.HasValue ? dishCompoundDish.Dish.Carbohydrates.Value : 0;
+                        var carbohydrates = dishCarbs * eatCompoundDish.Qty * dishCompoundDish.DishQty;
+                        currentProteinsSum += carbohydrates;
+
+                        var dishFats = dishCompoundDish.Dish.Fat.HasValue ? dishCompoundDish.Dish.Fat.Value : 0;
+                        var fats = dishFats * eatCompoundDish.Qty * dishCompoundDish.DishQty;
+                        currentFatSum += fats;
+
+                        var dishFibs = dishCompoundDish.Dish.Fiber.HasValue ? dishCompoundDish.Dish.Fiber.Value : 0;
+                        var fibers = dishFibs * eatCompoundDish.Qty * dishCompoundDish.DishQty;
+                        currentFiberSum += fibers;
+                    }
+                }
+
+                currentCaloriesSum += eatTypeCalories;
+
+                switch (eat.EatType)
+                {
+                    case EatTypeEnum.BREAKFAST:
+                        double breakfastKcalPer = GetEatTypeKcalPer(eatTypeCalories, parameters.BreakFastCalVal, parameters.BreakFastCalValExtra);
+                        breakfastKcal = GetEatTypeKcal(eatTypeCalories, parameters.BreakFastCalVal, parameters.BreakFastCalValExtra, breakfastKcalPer);
+
+                        break;
+
+                    case EatTypeEnum.SNACK1:
+                        double snack1KcalPer = GetEatTypeKcalPer(eatTypeCalories, parameters.Snack1CalVal, parameters.Snack1CalValExtra);
+                        snack1Kcal = GetEatTypeKcal(eatTypeCalories, parameters.Snack1CalVal, parameters.Snack1CalValExtra, snack1KcalPer);
+                        break;
+
+                    case EatTypeEnum.LUNCH:
+                        double luchKcalPer = GetEatTypeKcalPer(eatTypeCalories, parameters.LunchCalVal, parameters.LunchCalValExtra);
+                        lunchKcal = GetEatTypeKcal(eatTypeCalories, parameters.LunchCalVal, parameters.LunchCalValExtra, luchKcalPer);
+                        break;
+
+                    case EatTypeEnum.SNACK2:
+                        double snack2KcalPer = GetEatTypeKcalPer(eatTypeCalories, parameters.Snack2CalVal, parameters.Snack2CalValExtra);
+                        snack2Kcal = GetEatTypeKcal(eatTypeCalories, parameters.Snack2CalVal, parameters.Snack2CalValExtra, snack2KcalPer);
+                        break;
+
+                    case EatTypeEnum.DINNER:
+                        double dinnerKcalPer = GetEatTypeKcalPer(eatTypeCalories, parameters.DinnerCalVal, parameters.DinnerCalValExtra);
+                        dinnerKcal = GetEatTypeKcal(eatTypeCalories, parameters.DinnerCalVal, parameters.DinnerCalValExtra, dinnerKcalPer);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            bool isKcalAllOk = currentCaloriesSum > parameters.KCalMin && currentCaloriesSum <= parameters.KCalMax;
+
+            double proteinPer = currentProteinsSum * 100 / (parameters.KCalMax * 25 / 100);
+            bool isProteinsOk = proteinPer <= 100 && proteinPer > (12 * 100 / 25);
+
+            double carbohydratesPer = currentCarbohydratesSum * 100 / (parameters.KCalMax * 55 / 100);
+            bool isCarbohydratesOk = carbohydratesPer <= 100 && carbohydratesPer > 35 * 100 / 55;
+
+            double fatPer = currentFatSum * 100 / (parameters.KCalMax * 35 / 100);
+            bool isFatOk = fatPer <= 100 && fatPer > 20 * 100 / 35;
+
+            double fiberPer = currentFiberSum * 100 / 50;
+            bool isFiberOk = fiberPer <= 100 && fiberPer > 30 * 100 / 50;
+
+            return isKcalAllOk && isProteinsOk && isCarbohydratesOk && isFatOk && isFiberOk && breakfastKcal && snack1Kcal && lunchKcal && snack2Kcal && dinnerKcal;
+        }
+
         private double KCalOffSetVal
         {
             get => (_imc > 18.5 && _imc < 25) ? 100 : 500;
@@ -60,6 +182,16 @@ namespace MismeAPI.Service.Utils
         private double EatKCalValExtra(int percent)
         {
             return KCalOffSetVal * percent / 100;
+        }
+
+        private double GetEatTypeKcalPer(double calories, double calVal, double calValExtra)
+        {
+            return calories * 100 / (calVal + calValExtra);
+        }
+
+        private bool GetEatTypeKcal(double calories, double calVal, double calValExtra, double caloriesPer)
+        {
+            return calories >= (calVal - calValExtra) && caloriesPer < 101;
         }
     }
 }
