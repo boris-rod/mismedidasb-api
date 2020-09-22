459eeaf10135c1f946359ab4c91b30b1d097f5d9
Yoandry <mazinchess@gmail.com>
2020-09-22 14:18:19 -0400

    Add hand code.

--- MismeAPI/MismeAPI/Utils/DatabaseSeed.cs
+++ MismeAPI/MismeAPI/Utils/DatabaseSeed.cs
@@ -94,15 +94,14 @@ namespace MismeAPI.Utils
             handsCodes.Add(18, "D:/Projects/Mismes/BD Alimentos Saira/Alimentos 100x100/18.jpg");
 
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-            using (var package = new ExcelPackage(new FileInfo("D:/Projects/Mismes/BD Alimentos Saira/BASESOLIDONUEVA 2 recortada.xlsx")))
+            using (var package = new ExcelPackage(new FileInfo("D:/Projects/Mismes/BD Alimentos Saira/Envido  nuevos solo y para eliminar.xlsx")))
             {
                 var sheetCount = package.Workbook.Worksheets.Count;
-                var firstSheet = package.Workbook.Worksheets["Postres"];
-                for (int i = 301; i <= 398; i++)
+                var firstSheet = package.Workbook.Worksheets["Incluido nuevo"];
+                for (int i = 201; i <= 250; i++)
                 {
                     var code = firstSheet.Cells[i, 2].Text.Trim();
                     var dishName = firstSheet.Cells[i, 3].Text.Trim();
-
                     var handsCode = int.Parse(firstSheet.Cells[i, 4].Text.Trim());
 
                     var netWeight = double.Parse(firstSheet.Cells[i, 5].Text.Trim());
@@ -166,6 +165,7 @@ namespace MismeAPI.Utils
                     dish.Image = guid;
                     dish.Code = code;
                     dish.Name = dishName;
+                    dish.HandCode = handsCode;
                     if (netWeight >= 0)
                     {
                         dish.NetWeight = netWeight;
